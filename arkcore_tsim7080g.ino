c4209cfc9f6c1f7726dac45974174792a29bd576
internessi <50959344+internessi@users.noreply.github.com>
2025-06-11 18:48:56 +0200

    fist

--- arkcore_tsim7080g.ino
+++ arkcore_tsim7080g.ino
@@ -1,196 +1,245 @@
+/**
+ * @file      MinimalDeepSleepExample.ino
+ * @author    Lewis He (lewishe@outlook.com)
+ * @license   MIT
+ * @copyright Copyright (c) 2025  Shenzhen Xin Yuan Electronic Technology Co., Ltd
+ * @date      2025-01-22
+ *
+ */
 #include <Arduino.h>
-#include <NimBLEDevice.h>
-#include <Preferences.h>
-#include "soc/soc.h"
-#include "soc/rtc_cntl_reg.h"
+#define XPOWERS_CHIP_AXP2101
+#include "XPowersLib.h"
+#include "utilities.h"
 
-// BLE-Variablen
-static const NimBLEAdvertisedDevice* advDevice;
-static bool doConnect = false;
-static const uint32_t scanTimeMs = 5000;
-static uint32_t lastReadTime = 0;
-static const uint32_t readInterval = 10 * 60 * 1000;
+XPowersPMU  PMU;
 
-String SN;
-float temperature = 20;
-float pressure = 900;
-float humidity = 40;
-uint16_t radon = 10;
-const char* statusScanAranetRn = "";
-char printLine[20];
+// See all AT commands, if wanted
+#define DUMP_AT_COMMANDS
 
-uint16_t batteryVolts100 = 0; // interne Teilung, in 0.01 V (z. B. 331 = 3.31 V)
-const uint64_t sleepMinute  = 60ULL * 1000000ULL;
+#define TINY_GSM_RX_BUFFER 1024
 
-bool isSleepWakeup = false;
-byte nvsSensorCounter = 0;
-byte nvsGsmCounter = 0;
+#define TINY_GSM_MODEM_SIM7080
+#include <TinyGsmClient.h>
+
+// Set to wake up every 5 minutes to send positioning coordinates to the platform
+#define uS_TO_S_FACTOR 1000000ULL                       /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  20                             /* Time ESP32 will go to sleep (in seconds) */
 
 
-uint32_t unixTime = 1746345600;  // Platzhalter-Zeit bis erste Synchronisierung
-uint32_t syncMillis = 0;  // Zeitpunkt der letzten Synchronisierung (millis)
-uint32_t lastSendTime = 0;  // wird nach setup gesetzt
-uint8_t minutesLeft = 10;
-unsigned long lastMinuteTick = 0;
+#ifdef DUMP_AT_COMMANDS
+#include <StreamDebugger.h>
+StreamDebugger debugger(Serial1, Serial);
+TinyGsm        modem(debugger);
+#else
+TinyGsm        modem(SerialAT);
+#endif
+
+void getWakeupReason();
 
 
-#include <arkcore_tsim7070g.h>
-#include <aranet_rn.h>
-#include <epaper.h>
-#include <gsm_tsim7070g.h>
+void setup()
+{
 
-void switchResetReason() {
-  esp_reset_reason_t reason = esp_reset_reason();
+    Serial.begin(115200);
 
-  switch (reason) {
-    case ESP_RST_POWERON:
-      // Serial.println("Reset-Grund: Power-On oder Flash-Vorgang");
-      saveSensorDataToNVS();
-      break;
+    // Start while waiting for Serial monitoring
+    while (!Serial);
 
-    case ESP_RST_DEEPSLEEP:
-      isSleepWakeup = true;
-      loadCountersFromNVS();
-      nvsSensorCounter++;
-      nvsGsmCounter++; 
-      // Serial.println("Reset-Grund: Aufwachen aus Deep Sleep");
-      break;
+    delay(3000);
 
-    case ESP_RST_SW:
-    case ESP_RST_WDT:
-    case ESP_RST_PANIC:
-    case ESP_RST_BROWNOUT:
-      // Serial.println("Reset-Grund: Watchdog, Software-Reset, Panic oder Brownout");
-      break;
+    getWakeupReason();
 
-    default:
-      // Serial.println("Reset-Grund: Unbekannt oder sonstiger Grund");
-      break;
-  }
-}
+    Serial.println();
 
-void setup() {
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);  // Brownout-Detector deaktivieren
-  delay(100);
-  pinMode(0, INPUT);
-  pinMode(2, INPUT);
-  pinMode(12, INPUT); // blaue LED
-  pinMode(13, INPUT);
-  pinMode(15, INPUT);
-  pinMode(35, INPUT); // Spannungsteiler
-  pinMode(MODEM_STATUS, INPUT); //  nur lesen
-  pinMode(MODEM_PWRKEY, INPUT);
-  //pinMode(MODEM_PWRKEY, OUTPUT); // Modem PWRKEY
-  //digitalWrite(MODEM_PWRKEY, HIGH);  // PWRKEY = HIGH = AUS !
-  //pinMode(MODEM_PWRKEY, OUTPUT);
-  //digitalWrite(MODEM_PWRKEY, LOW);
-
-
-  Serial.begin(115200);
-  delay(100);
-
-  SN = generateSerialFromChipId();
-  //SN = "RGZ9R"; // nur für mein Testmodell !!!!
-
-  switchResetReason();
-  if (!isSleepWakeup) {
-    delay(1000);
-    ePaperInit();
-    loadSensorDataFromNVS();
-    showTextInRegion("RADOCON", 3, 22);   
-    showTextInRegion(SN.c_str(), 151, 22);
-    blinkBlueLed3x();
-    showTextInRegion("Reset", 56, 12);   
-    delay(1000);   
-    showTextInRegion("ALPHA", 83, 12);     
-    showTextInRegion("FW V0.6", 110, 12);     
-    delay(2000);
-    testAndShutdownModem();
-    blinkBlueLed3x();
-    delay(8000);
-  }
-
-  Serial.println("");
-  if (isSleepWakeup) {  
-    Serial.println("Reset-Grund: Aufwachen aus Deep Sleep");
-  } else {
-    Serial.println("Reset-Grund: Anderer Grund");
-  }
-  
-  // BLE Sensor Reading (nur alle 9 Minuten)
-  if (!isSleepWakeup || (nvsSensorCounter == 9)) {
-    scanSensor();
-    delay(100);
-    readSensor();
-    if (humidity == 0) {
-      loadSensorDataFromNVS();
+    /*********************************
+     *  step 1 : Initialize power chip,
+     *  turn on modem and gps antenna power channel
+    ***********************************/
+    if (!PMU.begin(Wire, AXP2101_SLAVE_ADDRESS, I2C_SDA, I2C_SCL)) {
+        Serial.println("Failed to initialize power.....");
+        while (1) {
+            delay(5000);
+        }
     }
-    //NimBLEDevice::deinit(true);
-    saveSensorDataToNVS();
-    nvsSensorCounter = 0;
-  }
+
+    // Set the working voltage of the modem, please do not modify the parameters
+    PMU.setDC3Voltage(3000);    // SIM7080 Modem main power channel 2700~ 3400V
+    PMU.enableDC3();
+
+    // Modem GPS Power channel
+    PMU.setBLDO2Voltage(3300);
+    PMU.enableBLDO2();      // The antenna power must be turned on to use the GPS function
+
+    // TS Pin detection must be disable, otherwise it cannot be charged
+    PMU.disableTSPinMeasure();
 
 
+    /*********************************
+     * step 2 : start modem
+    ***********************************/
 
-  loadSensorDataFromNVS();
-  ePaperInit();
+    Serial1.begin(115200, SERIAL_8N1, BOARD_MODEM_RXD_PIN, BOARD_MODEM_TXD_PIN);
 
-  // Display: FULL REFRESH nur bei BLE-Messung oder GSM-Senden
-  //if (!isSleepWakeup || (nvsSensorCounter == 9) || (nvsGsmCounter == 54)) {
-    
-    drawScreen();  // Komplettes Display mit Sensordaten
-    snprintf(printLine, sizeof(printLine), "%u Bq", radon);
-    showTextInRegion(printLine, 35, 22);     
-    showTextInRegion(SN.c_str(), 151, 22);
-  //} 
-  // else -> PARTIAL REFRESH (nur Countdown/Akku, kein drawScreen())
+    pinMode(BOARD_MODEM_PWR_PIN, OUTPUT);
+    pinMode(BOARD_MODEM_DTR_PIN, OUTPUT);
+    pinMode(BOARD_MODEM_RI_PIN, INPUT);
 
-  // Batterie-Status (immer)
-  measureBattery();
-  if (batteryVolts100 == 500) {
-    snprintf(printLine, sizeof(printLine), "Akku laden");
-  } else if (batteryVolts100 < 330) {
-    snprintf(printLine, sizeof(printLine), "Akku leer");
-  } else {
-    snprintf(printLine, sizeof(printLine), "Akku %.2fV", batteryVolts100 / 100.0);
-  }
-  showTextInRegion(printLine, 126, 12);
 
-  // GSM Senden (nur bei guter Batterie und alle 54 Minuten)
-  //batteryVolts100 = 320;
-  if (batteryVolts100 > 330 && (!isSleepWakeup || nvsGsmCounter == 54)) {
-    loadSensorDataFromNVS();
-    pressure = batteryVolts100 + 400; // temporär batterie senden
-    sendSensorDataViaGSM();
-    nvsGsmCounter = 0;
-  }
+    /**
+     *  When the user sets "AT+CSCLK=1", pull up the DTR pin,
+     *  the module will automatically enter the sleep mode.
+     *  At this time, the serial port function cannot communicate normally.
+     *  Pulling DTR low in this mode can wake up the module
+     */
+    digitalWrite(BOARD_MODEM_DTR_PIN, LOW);
 
-  // Countdown (immer)
-  snprintf(printLine, sizeof(printLine), "Messen in %2um", 9-nvsSensorCounter);
-  showTextInRegion(printLine, 78, 12);    
-  snprintf(printLine, sizeof(printLine), "Senden in %2um", 54-nvsGsmCounter);
-  showTextInRegion(printLine, 102, 12);
+    int retry = 0;
+    while (!modem.testAT(1000)) {
+        Serial.print(".");
+        if (retry++ > 6) {
+            // Pull down PWRKEY for more than 1 second according to manual requirements
+            digitalWrite(BOARD_MODEM_PWR_PIN, LOW);
+            delay(100);
+            digitalWrite(BOARD_MODEM_PWR_PIN, HIGH);
+            delay(1000);
+            digitalWrite(BOARD_MODEM_PWR_PIN, LOW);
+            retry = 0;
+            Serial.println("Retry start modem .");
+        }
+    }
+    Serial.println();
+    Serial.print("Modem started!");
 
-  display.hibernate();  // Display in tiefsten Schlafmodus
-  SPI.end();
+    // Keep running for 10 seconds
+    uint32_t runAt = millis() + 10000;
+    while (runAt > millis()) {
+        while (Serial1.available()) {
+            Serial.write(Serial1.read());
+        }
+        while (Serial.available()) {
+            Serial1.write(Serial.read());
+        }
+    }
 
-  Serial.println("Deep Sleep...");
-  saveCountersToNVS();
+    // You can turn off the modem or PMU power supply voltage
+    // It can also keep the SIM7080G powered
+    // modem.poweroff();
 
-  //digitalWrite(MODEM_PWRKEY, HIGH); // PWRKEY = HIGH = aus
-  
-  //pinMode(MODEM_PWRKEY, INPUT);        // entkoppeln
-  //pinMode(MODEM_PWRKEY, OUTPUT);   // bewusst definieren
-  //digitalWrite(MODEM_PWRKEY, HIGH); // halten
-  delay(100);                      // stabilisieren
-  
-  //delay(54000); // Alternativer sleep mit viel verbrauch
-  //esp_sleep_enable_timer_wakeup(1000000ULL); // 1 Sekunde Deep Sleep
-  
-  esp_sleep_enable_timer_wakeup(1 * sleepMinute);
-  esp_deep_sleep_start();
+    // PMU.disableDC3();
+    // PMU.disableBLDO2();
 
-  //lastSendTime = millis(); remember the variable you will need it
+    int delaySeconds = 5;
+    while (delaySeconds--) {
+        Serial.println("Will enter to deep sleep after ");
+        Serial.print(delaySeconds);
+        Serial.println(" seconds");
+        delay(1000);
+    }
+
+
+    /*
+    First we configure the wake up source
+    We set our ESP32 to wake up every 5 seconds
+    */
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+    Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
+                   " Seconds");
+
+    /*
+    Next we decide what all peripherals to shut down/keep on
+    By default, ESP32 will automatically power down the peripherals
+    not needed by the wakeup source, but if you want to be a poweruser
+    this is for you. Read in detail at the API docs
+    http://esp-idf.readthedocs.io/en/latest/api-reference/system/deep_sleep.html
+    Left the line commented as an example of how to configure peripherals.
+    The line below turns off all RTC peripherals in deep sleep.
+    */
+    // esp_deep_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
+    // Serial.println("Configured all RTC Peripherals to be powered down in sleep");
+
+    /*
+    Now that we have setup a wake cause and if needed setup the
+    peripherals state in deep sleep, we can now start going to
+    deep sleep.
+    In the case that no wake up sources were provided but deep
+    sleep was started, it will sleep forever unless hardware
+    reset occurs.
+    */
+    Serial.println("Going to sleep now");
+    Serial.flush();
+    esp_deep_sleep_start();
+    Serial.println("This will never be printed");
+
 }
 
-void loop() {}
+void loop()
+{
+}
+
+
+
+void getWakeupReason()
+{
+    esp_sleep_wakeup_cause_t wakeup_reason;
+
+    wakeup_reason = esp_sleep_get_wakeup_cause();
+
+    switch (wakeup_reason) {
+    case ESP_SLEEP_WAKEUP_UNDEFINED:
+        //!< In case of deep sleep, reset was not caused by exit from deep sleep
+        Serial.println("In case of deep sleep, reset was not caused by exit from deep sleep");
+        break;
+    case ESP_SLEEP_WAKEUP_ALL:
+        //!< Not a wakeup cause: used to disable all wakeup sources with esp_sleep_disable_wakeup_source
+        Serial.println("Not a wakeup cause: used to disable all wakeup sources with esp_sleep_disable_wakeup_source");
+        break;
+    case ESP_SLEEP_WAKEUP_EXT0:
+        //!< Wakeup caused by external signal using RTC_IO
+        Serial.println("Wakeup caused by external signal using RTC_IO");
+        break;
+    case ESP_SLEEP_WAKEUP_EXT1:
+        //!< Wakeup caused by external signal using RTC_CNTL
+        Serial.println("Wakeup caused by external signal using RTC_CNTL");
+        break;
+    case ESP_SLEEP_WAKEUP_TIMER:
+        //!< Wakeup caused by timer
+        Serial.println("Wakeup caused by timer");
+        break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD:
+        //!< Wakeup caused by touchpad
+        Serial.println("Wakeup caused by touchpad");
+        break;
+    case ESP_SLEEP_WAKEUP_ULP:
+        //!< Wakeup caused by ULP program
+        Serial.println("Wakeup caused by ULP program");
+        break;
+    case  ESP_SLEEP_WAKEUP_GPIO:
+        //!< Wakeup caused by GPIO (light sleep only)
+        Serial.println("Wakeup caused by GPIO (light sleep only)");
+        break;
+    case ESP_SLEEP_WAKEUP_UART:
+        //!< Wakeup caused by UART (light sleep only)
+        Serial.println("Wakeup caused by UART (light sleep only)");
+        break;
+    case ESP_SLEEP_WAKEUP_WIFI:
+        //!< Wakeup caused by WIFI (light sleep only)
+        Serial.println("Wakeup caused by WIFI (light sleep only)");
+        break;
+    case ESP_SLEEP_WAKEUP_COCPU:
+        //!< Wakeup caused by COCPU int
+        Serial.println("Wakeup caused by COCPU int");
+        break;
+    case ESP_SLEEP_WAKEUP_COCPU_TRAP_TRIG:
+        //!< Wakeup caused by COCPU crash
+        Serial.println("Wakeup caused by COCPU crash");
+        break;
+    case  ESP_SLEEP_WAKEUP_BT:
+        //!< Wakeup caused by BT (light sleep only)
+        Serial.println("Wakeup caused by BT (light sleep only)");
+        break;
+    default :
+        Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason);
+        break;
+
+    }
+}
